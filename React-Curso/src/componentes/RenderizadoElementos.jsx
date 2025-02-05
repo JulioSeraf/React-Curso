928fbdce58ebacca5d1c5d570b9605fbd8b4236e
julio serafim <juliodev26@gmail.com>
2025-02-05 15:54:26 +0100

    Renderizacion de Elementos

--- React-Curso/src/componentes/RenderizadoElementos.jsx
+++ React-Curso/src/componentes/RenderizadoElementos.jsx
@@ -1,5 +1,16 @@
 import React,{Component} from "react";
+import data from "../helpers/RenderizadoElementos.json";
 
+console.log(data)
+
+function ElementoLista(props){
+    return(
+        <li>
+         <a target="_blank" href={props.el.web}>{props.el.name}</a>
+        </li>
+    )
+    
+}
 export default class RederizadoElementos extends Component{
     constructor(props){
         super(props);
@@ -11,7 +22,11 @@ export default class RederizadoElementos extends Component{
     render(){
         return <div>
             <h2>Renderizado Elementos</h2>
-            <ul>{this.state.persona.map((el)=>{<li>{el}</li>})}</ul>
+            <ul>{this.state.persona.map((el, index)=> <li key={index}>{el}</li>)}</ul>
+            <h3>Rederizado con json, frameworks mais populares</h3>
+            <ol>
+                {data.frameworks.map((el) => <ElementoLista key={el.id} el={el}/>)}
+            </ol>
         </div>
     }
-}
+};
